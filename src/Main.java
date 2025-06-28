57c0f95539037054154606fa7dc568d8be4f163f
Andres Daza <felipedazasernadim@gmail.com>
2025-06-27 20:14:11 -0500

    Agregar clase Main con estructura inicial

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Book> books = new ArrayList<>();
+
+        System.out.println("Tienda de Libros - Java Streams");
+    }
+}
